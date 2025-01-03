6e62b74bfe546811c32d44175ddbd6d5e8fe7a67
lepidos <lepidos@asteel.WORKGROUP>
2025-01-03 12:01:29 +0000

    Exerc 5: NAT

--- nat.sh
+++ nat.sh
@@ -2,4 +2,4 @@ iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
 
 iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination 192.168.200.21
 #ou
-iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+sudo iptables -t nat -A PREROUTING -p tcp --dport 21 -j DNAT --to-destination 192.168.200.21
