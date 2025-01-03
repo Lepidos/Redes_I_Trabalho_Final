745af8263075553516b0d84323dd01e4b8120ac6
lepidos <lepidos@asteel.WORKGROUP>
2025-01-03 14:37:40 +0000

    fim, espero eu

--- nat.sh
+++ nat.sh
@@ -1,5 +1,6 @@
-iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
+
+iptables -t nat -A POSTROUTING -s 192.168.200.0/24 -j MASQUERADE
 
 iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination 192.168.200.21
-#ou
-sudo iptables -t nat -A PREROUTING -p tcp --dport 21 -j DNAT --to-destination 192.168.200.21
+
+iptables -t nat -A PREROUTING -p tcp --dport 21 -j DNAT --to-destination 192.168.200.21
